0e48ef82c32a65c4cb1e1c463a11e449ce9c0c86
Єгембердінов Антон <118931658+KarazingaBazinga@users.noreply.github.com>
2024-03-25 16:30:53 +0200

    Add user_profile

--- db/migrate/20240325103243_add_confirmable_to_users.rb
+++ db/migrate/20240325103243_add_confirmable_to_users.rb
@@ -13,7 +13,7 @@ class AddConfirmableToUsers < ActiveRecord::Migration[7.1]
     remove_column :users, :confirmation_token
     remove_column :users, :confirmed_at
     remove_column :users, :confirmation_sent_at
-    remove_column :users,   :unconfirmed_email
+    remove_column :users, :unconfirmed_email
     remove_index :users, :confirmation_token
   end
 end
